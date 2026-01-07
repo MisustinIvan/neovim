563885416ab23bde608218ea8a34d39ab603f140
MisustinIvan <ivanmis596@gmail.com>
2026-01-07 09:14:00 +0100

    updates to the diary feature

--- lua/config/diary.lua
+++ lua/config/diary.lua
@@ -23,6 +23,13 @@ local function ensure_dir(path)
 	vim.fn.mkdir(path, "p")
 end
 
+local function buffer_is_empty(buf)
+	if vim.api.nvim_buf_line_count(buf) ~= 1 then
+		return false
+	end
+	return vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] == ''
+end
+
 function M:open_diary_today()
 	ensure_dir(self.path)
 
@@ -32,7 +39,15 @@ function M:open_diary_today()
 		return
 	end
 
-	vim.cmd.edit(self.path .. "/" .. date .. ".md")
+	local today_path = self.path .. '/' .. date .. '.md'
+
+	vim.cmd.edit(today_path)
+
+	local buf = vim.api.nvim_get_current_buf()
+
+	if buffer_is_empty(buf) then
+		vim.api.nvim_put({ date }, 'l', false, true)
+	end
 end
 
 function M:open_diary_list()
