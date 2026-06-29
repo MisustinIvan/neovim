63cab7440eb4e3ea8245a0a15671f91f83443810
MisustinIvan <ivanmis596@gmail.com>
2026-06-29 11:38:23 +0200

    update treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,26 +5,29 @@ vim.pack.add({
 	},
 })
 
-local filetypes = {
-	'go',
-	'lua',
-	'html',
-	'javascript',
-	'css',
-	'csv',
-	'tsv',
-	'sql',
-}
+local ts = require("nvim-treesitter")
+ts.setup({
+	install_dir = vim.fn.stdpath("data") .. "/site"
+})
 
-require('nvim-treesitter').setup({
-	ensure_installed = filetypes,
-	highlight = {
-		enable = true,
-	},
-	indent = {
-		enable = true,
-	},
-	auto_install = true,
+local filetypes = { 'go', 'lua', 'html', 'javascript', 'css', 'csv', 'tsv', 'sql' }
+local installed = require("nvim-treesitter.config").get_installed()
+
+local to_install = vim.iter(filetypes):filter(function(lang)
+	return not vim.tbl_contains(installed, lang)
+end):totable()
+
+if #to_install > 0 then
+	ts.install(to_install)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("ts_core_setup", { clear = true }),
+	callback = function(ev)
+		pcall(vim.treesitter.start, ev.buf)
+
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
 })
 
 vim.api.nvim_create_autocmd('PackChanged', {
